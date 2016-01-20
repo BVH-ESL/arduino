efb99f6308b71d1c4577abb61b69341835f06d53
batman1292 <batman12922@gmail.com>
2016-01-20 18:57:02 +0700

    control servo with gy-521

---
+++ motoGogo/controllservoformgy951/controllservoformgy951.ino
@@ -0,0 +1,45 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+SoftwareSerial mySerial(9, 8); // RX, TX
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+float yaw;
+
+Servo myservo; 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  mySerial.begin(57600);
+  myservo.attach(10); 
+}
+
+void loop() {
+  serialEvent(); //call the function
+  // print the string when a newline arrives:
+  if (stringComplete) {
+//    Serial.println(inputString);
+//    Serial.print("index , as \t");Serial.println(inputString.indexOf(','));
+    yaw = inputString.substring(inputString.indexOf('=')+1, inputString.indexOf(',')).toFloat();
+    // clear the string:
+    if(yaw > 0){
+      myservo.write(yaw);
+    }
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+void serialEvent() {
+  while (mySerial.available()) {
+    char inChar = (char)mySerial.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
