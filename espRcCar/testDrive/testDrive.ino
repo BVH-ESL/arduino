a4380fa6ef64b44f9bc0805a603b82e8a1ff668d
batman1292 <batman12922@gmail.com>
2016-03-09 18:35:28 +0700

    edit

--- espRcCar/testDrive/testDrive.ino
+++ espRcCar/testDrive/testDrive.ino
@@ -8,6 +8,7 @@ int in2 = 12;
 int enB = 2;
 int in3 = 5;
 int in4 = 4;
+double motorSpeed = 0.7;
 void setup()
 {
   // set all the motor control pins to outputs
@@ -25,19 +26,27 @@ void demoOne()
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   // set speed to 200 out of possible range 0~255
-  analogWrite(enA, 1023);
+  analogWrite(enA, 1023*motorSpeed);
   // turn on motor B
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   // set speed to 200 out of possible range 0~255
-  analogWrite(enB, 1023);
-  delay(2000);
+  analogWrite(enB, 1023*motorSpeed);
+  delay(1000);
+  
+  analogWrite(enA, 1023*0);
+  analogWrite(enB, 1023*0);
+  delay(1000);
   // now change motor directions
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);  
+  
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH); 
-  delay(2000);
+  delay(1000);
+  analogWrite(enA, 1023*0.7);
+  analogWrite(enB, 1023*0.7);
+  delay(1000);
   // now turn off motors
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);  
